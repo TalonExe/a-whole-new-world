bd86d10090d4fa0306d43766125511703d4fd2d1
TalonExe <liew321@hotmail.com>
2025-09-06 09:44:01 +0800

    Initial Commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+	import '../app.css';
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+	export const prerender = true;
+	export const trailingSlash = "always";
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+{@render children?.()}
